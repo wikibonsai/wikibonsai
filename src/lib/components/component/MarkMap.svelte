10a390d9994f0403179f98b6cacb592b67413ddf
manunamz <manunamz@pm.me>
2024-05-30 16:41:33 -0400

    fix: bonsai trees text color based on theming.

--- src/lib/components/component/MarkMap.svelte
+++ src/lib/components/component/MarkMap.svelte
@@ -95,7 +95,7 @@
 
 <svg id="markmap"
      bind:this={markmap}
-     style={`height: ${height}vh; width: ${width}vw;`}
+     style={`height: ${height}vh; width: ${width}vw; color: var(--text-color);`}
      xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink">
 </svg>
