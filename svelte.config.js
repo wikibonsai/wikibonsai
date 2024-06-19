a9422ce9559719bef1196fcd91830338e6253909
manunamz <manunamz@pm.me>
2024-06-19 11:40:16 -0400

    ref: attempt to add sitemap.

--- svelte.config.js
+++ svelte.config.js
@@ -4,24 +4,31 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+  extensions: ['.svelte', '.md'],
 
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			// this is for things like tailwind..
-			// postcss: true,
-		}),
-		vitePreprocess({})
-	],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      // this is for things like tailwind..
+      // postcss: true,
+    }),
+    vitePreprocess({})
+  ],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      fallback: 'index.html',
+      precompress: false,
+      sitemapOptions: {
+        // Include all routes in the sitemap.xml
+        includedRoutes: async (routes) => routes,
+      },
+    })
+  }
 };
 
 export default config;
